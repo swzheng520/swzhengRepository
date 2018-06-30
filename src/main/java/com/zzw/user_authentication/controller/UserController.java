a2c6dc2a1a239f4c2c70b719b4c90e95f37e3080
zhengzhiwei <zhiwei.zheng@anxintrust.com>
2018-06-30 16:38:08 +0800

    添加controller

---
+++ src/main/java/com/zzw/user_authentication/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.zzw.user_authentication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@GetMapping(value = "/getUser")
+	public String getUser() {
+		return "lisi";
+	}
+
+}
